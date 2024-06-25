23b00ba2e805b0420acc4c948c16f7d9303523be
gabiten03 <gabiten@gmail.com>
2024-06-25 11:51:28 -0300

    14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { Spacer } from '@chakra-ui/react'
+
 import Header from './components/Header/Header';
 
 import Portfolio from './components/Portfolio/Portfolio';
-import PortfolioItem from './components/PortfolioItem/PortfolioItem'
+
 import Landpage from './components/LandPage/Landpage';
 import MeHi from './components/MeHi/MeHi';
-import CardsContainer from './components/CardsContainer/CardsContainer';
+
 
 import {
   BrowserRouter as Router,
